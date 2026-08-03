0287e8ede6354ba1958a7da93782354418d398e4
Pyarasani Uday Kumar <udaypyarasani02@gmail.com>
2026-08-03 21:24:11 +0530

    Time: 4 ms (40.63%) | Memory: 44 MB (90.27%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int mostWordsFound(String[] n) {
+        int max = 0;
+
+        for (int i = 0; i < n.length; i++) {
+
+            int count = 1;
+
+            for (int j = 0; j < n[i].length(); j++) {
+                if (n[i].charAt(j) == ' ') {
+                    count++;
+                }
+            }
+
+            if (count > max) {
+                max = count;
+            }
+        }
+        return max;
+    }
+}
