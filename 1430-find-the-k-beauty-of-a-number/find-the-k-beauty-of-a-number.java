5137f347301df1e6e1c4155b8c5a0dd34d8b3635
Pyarasani Uday Kumar <udaypyarasani02@gmail.com>
2026-08-05 23:12:05 +0530

    Time: 1 ms (92.54%) | Memory: 42.1 MB (75.12%) - LeetSync

---
+++ 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        String str = String.valueOf(num);
+        int count=0;
+        for(int i=0;i<=str.length()-k;i++){
+            int n = Integer.parseInt(str.substring(i, i + k));
+            if(n!=0 && num%n==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
