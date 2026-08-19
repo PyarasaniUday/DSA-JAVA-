386cc913741494583d0f13c3d9a86f5f1f3327df
Pyarasani Uday Kumar <udaypyarasani02@gmail.com>
2026-08-19 21:08:37 +0530

    Time: 3 ms (4.96%) | Memory: 43.2 MB (15.32%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        /*
         Arrays.sort(stones);
         ArrayList<Integer> list = new ArrayList<>();
         for (int stone : stones) {
@@ -19,40 +18,5 @@ class Solution {
             }
         }
         return list.isEmpty() ? 0 : list.get(0);
-
-        */
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        // Add all stones to ArrayList
-        for (int x : stones) {
-            list.add(x);
-        }
-
-        while (list.size() > 1) {
-
-            // Sort the list
-            Collections.sort(list);
-
-            // Get last two largest stones
-            int x = list.get(list.size() - 1);
-            int y = list.get(list.size() - 2);
-
-            // Remove both
-            list.remove(list.size() - 1);
-            list.remove(list.size() - 1);
-
-            // If they are not equal, add difference
-            if (x != y) {
-                list.add(x - y);
-            }
-        }
-
-        // If list is empty, return 0
-        if (list.size() == 0)
-            return 0;
-
-        return list.get(0);
-    
     }
 }
