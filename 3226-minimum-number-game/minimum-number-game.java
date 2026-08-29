b7504f404514ab3ea5958f0577d495aa82cc0441
Pyarasani Uday Kumar <udaypyarasani02@gmail.com>
2026-08-29 13:36:51 +0530

    Time: 6 ms (31.61%) | Memory: 46.7 MB (86.64%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            list.add(nums[i]);
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+        while(list.size()>1){
+            Collections.sort(list);
+            int x=list.get(0);
+            int y=list.get(1);
+            list.remove(0);
+            list.remove(0);
+            res.add(y);
+            res.add(x);
+        }
+        int arr[]=new int[res.size()];
+        for(int i=0;i<res.size();i++){
+            arr[i]=res.get(i);
+        }
+
+        return arr;
+    }
+}
