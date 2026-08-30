e24a57a1930e70fc34d047c0015dca6912dd3d60
Pyarasani Uday Kumar <udaypyarasani02@gmail.com>
2026-08-30 17:41:28 +0530

    Time: 2 ms (72.01%) | Memory: 47.1 MB (73.27%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int maxProduct(int[] nums) {
         int n=nums.length;
         int res=nums[0];
@@ -13,5 +13,34 @@ class Solution {
             }
         }
         return res;
+    }
+}
+*/
+
+//sw 
+
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        int max = nums[0];
+        int currentMax = nums[0];
+        int currentMin = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int num = nums[i];
+
+            int tempMax = currentMax;
+
+            currentMax = Math.max(num,
+                    Math.max(currentMax * num, currentMin * num));
+
+            currentMin = Math.min(num,
+                    Math.min(tempMax * num, currentMin * num));
+
+            max = Math.max(max, currentMax);
+        }
+
+        return max;
     }
 }
