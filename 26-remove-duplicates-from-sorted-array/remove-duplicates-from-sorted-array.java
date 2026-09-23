bf5bef8d6f5b06dcb2180686b58ebce2ef63c45f
Pyarasani Uday Kumar <udaypyarasani02@gmail.com>
2026-09-23 21:21:36 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (91.14%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int removeDuplicates(int[] nums) {
 
         ArrayList<Integer> res = new ArrayList<>();
@@ -17,5 +17,22 @@ class Solution {
         }
 
         return res.size();
+    }
+}
+*/
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int i = 0;
+
+        for(int j = 1; j < nums.length; j++) {
+
+            if(nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
     }
 }
